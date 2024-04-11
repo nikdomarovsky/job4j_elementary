0dbe3fc1c2c6f394ac2ba62d8120da735a78db90
Администратор <nikdomarovsky@gmail.com>
2024-04-11 23:12:35 +0300

    Изменение в файлах

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -3,8 +3,6 @@ package ru.job4j;
 public class Echo {
     public static void main(String[] args) {
         System.out.println("This is echo");
-        System.out.println("This is echo");
-
     }
 
 }

95a518ed85b62461e0b7ef2a2b912cb2466ef31e
Администратор <nikdomarovsky@gmail.com>
2024-05-15 13:45:22 +0300

    5.7. Ипотека [#156315 #495930]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-        amount += (amount * (percent / 100)) - salary;
-        year++;
+            amount += (amount * (percent / 100)) - salary;
+            year++;
         }
         return year;
     }

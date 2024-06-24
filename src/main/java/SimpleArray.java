ed4d0f37f496f3bb6d3f556da9c31980a92eea35
Администратор <nikdomarovsky@gmail.com>
2024-06-24 17:40:14 +0300

    Выводит каждого элемента в консоль

---
+++ src/main/java/SimpleArray.java
@@ -0,0 +1,9 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Элемент массива с индексом " + i + " равен " + numbers[i]);
+        }
+    }
+}
